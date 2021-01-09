038eca6c7bd2df5570e876b07a7de0532894edbc
Oluwole <oluwole@Oluwoles-MacBook-Pro.local>
2021-01-09 13:35:23 +0100

    added endpoint to start a new game

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import * as restify from 'restify';
 import * as plugins from 'restify-plugins';
 import { config } from './config/config';
 
+// service locator via dependency injection
+const serviceLocator = require('./config/di');
 const server = restify.createServer({
     name: config.app_name,
     versions: ['1.0.0'],
@@ -15,6 +17,10 @@ server.use(plugins.acceptParser(server.acceptable));
 server.use(plugins.queryParser());
 server.use(plugins.bodyParser());
 
+// setup Routing and Error Event Handling
+import { setup } from './routes/index';
+
+setup(server, serviceLocator);
 server.listen(config.port, () => {
     console.log(`${config.app_name} listening at ${config.port}`);
   });
---
+++ src/routes/index.ts
@@ -0,0 +1,29 @@
+import * as bodyParser from 'body-parser';
+
+export const setup = (server, serviceLocator) => {
+  const mainController = serviceLocator.get('mainController');
+
+  // parse application/x-www-form-urlencoded
+  server.use(bodyParser.urlencoded({ extended: false }));
+
+  // parse application/json
+  server.use(bodyParser.json());
+
+  server.get(
+    {
+      path: '/',
+      name: 'app health check',
+      version: '1.0.0',
+    },
+    (req, res) => res.send('Welcome to the Test API Service')
+  );
+  // create game sesssion
+  server.post(
+    {
+      path: '/game/start',
+      name: 'start game vehicle data',
+      version: '1.0.0',
+    },
+    (req, res) => mainController.startGame(req, res)
+  );
+};
